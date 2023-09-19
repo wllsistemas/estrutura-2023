2bb8855a44de34a802743d939e680f6237e1d3b6
WILLIAM FRANCISCO LEITE <61367053+wllsistemas@users.noreply.github.com>
2023-09-19 20:28:35 -0300

    aula 1

---
+++ aula_1/index.php
@@ -0,0 +1,10 @@
+<?php 
+
+echo '<h1>teste1</h1>';
+echo '<h1>teste2</h1>';
+echo '<h1>teste3</h1>';
+echo '<h1>teste4</h1>';
+echo '<h1>teste5</h1>';
+
+echo '<span>teste6</span><br>';
+echo '<span>teste7</span>';
