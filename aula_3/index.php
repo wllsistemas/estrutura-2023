16de8428cd8924921bb20a006b11e60739707acc
WILLIAM FRANCISCO LEITE <61367053+wllsistemas@users.noreply.github.com>
2023-09-26 20:32:21 -0300

    aula 3

---
+++ aula_3/index.php
@@ -0,0 +1,30 @@
+<?php
+
+// + SOMA
+// - SUBTRAÇÃO
+// * MULTIPLICAÇÃO
+// / MULTIPLICAÇÃO
+// % MOD = MÓDULO
+
+// compara somente valores
+$valor1 == 10;
+
+// compara valores e o type das variáveis
+$valor1 === 10;
+
+// atribuição simples
+$valor2 = 10;
+$valor3 = 20;
+
+$resultado = 1;
+
+$resultado += $valor2 + $valor3;
+
+$resultado = $resultado + ($valor2 + $valor3);
+
+echo $resultado . '<br>';
+
+echo 'Resultado da divisão: ' .  ($valor2 % $valor3);
+
+
+
