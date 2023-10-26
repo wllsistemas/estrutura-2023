ca8d613ed620a240a44fc893bcd6a604b0b3f971
WILLIAM FRANCISCO LEITE <61367053+wllsistemas@users.noreply.github.com>
2023-10-26 18:50:04 -0300

    atividade 1

---
+++ aula_10/conta.php
@@ -0,0 +1,30 @@
+<?php
+
+$saldo_inicial_corrente = 1000;
+$saldo_inicial_poupanca = 500;
+
+$movimentacoes = [];
+
+$movimentacao = [
+    "tipo" => "CRÉDITO",
+    "conta"=> "CORRENTE",
+    "valor_movimento" => 100
+];
+
+$movimentacao = [
+    "tipo" => "DÉBITO",
+    "conta"=> "POUPANÇA",
+    "valor_movimento" => 50
+];
+
+$movimentacao = [
+    "tipo" => "DÉBITO",
+    "conta"=> "POUPANÇA",
+    "valor_movimento" => 500
+];
+
+$movimentacao = [
+    "tipo" => "DÉBITO",
+    "conta"=> "CORRENTE",
+    "valor_movimento" => 10
+];
