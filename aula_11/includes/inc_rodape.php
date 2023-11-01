6bd740e2b4bed18918bfcdd544a56fb219ee09b3
WILLIAM FRANCISCO LEITE <61367053+wllsistemas@users.noreply.github.com>
2023-10-31 21:42:03 -0300

    aula 11

---
+++ aula_11/includes/inc_rodape.php
@@ -0,0 +1,3 @@
+<?php
+
+echo 'rodapé<br>';
---
+++ aula_11/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php include_once 'includes/inc_topo.php';?>
+    <h2>corpo</h2>
+    <?php require_once 'includes/inc_rodape.php'; ?>
+</body>
+</html>
