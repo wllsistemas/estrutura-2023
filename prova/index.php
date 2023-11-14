75143c0e6d9046c91d4ba12f3f751ac4ef1659d4
WILLIAM FRANCISCO LEITE <61367053+wllsistemas@users.noreply.github.com>
2023-11-14 19:00:29 -0300

    prova

--- prova/index.php
+++ prova/index.php
@@ -77,6 +77,15 @@ $aluno3 = [
 ];
 $alunos[] = $aluno3;
 
+$soma_idade = 0;
+$qtde_alunos = 0;
+foreach($alunos as $aluno):
+    $soma_idade += $aluno['idade'];
+    $qtde_alunos++;
+endforeach;
+
+echo "3 - MÉDIA DE IDADE " . ($soma_idade / $qtde_alunos);
+
 // 1 - DESCREVA QUANDO PODEMOS USAR "ELSE" OU "ELSEIF" EM UMA INSTRUÇÃO IF
 // 2 - DESCREVA CASOS DE USO PARA "&&" (AND) E "||" (OR) NA INSTRUÇÃO IF
 // 3 - EXIBA MÉDIA DE IDADE DOS ALUNOS
